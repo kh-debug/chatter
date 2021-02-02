30da52ec489de0cce859e8d61d1801e6d8db094a
kh-debug <kmhorton@uw.edu>
2021-02-01 20:51:09 -0800

    added messages to a list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+
+import {useState} from 'react';
 import './App.css';
+import TextInput from './TextInput';
 
 function App() {
-  return <div className="App"> 
+  const [messages,setMessages] = useState([{text:'hello'}])
+
+  console.log(messages)
+
+  return <div className="App">
     <header className="header">
-      <div className="logo"/>
+      <div className="logo" />
       CHATTER
     </header>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <div key={i} className="message-row">
+          <div className="message">
+            {m.text}
+          </div>
+        </div>
+      })}
+    </main>
+
+    <TextInput
+      send={(t)=> setMessages( [...messages, {text:t}] )}
+    />
+
   </div>
 }
 
