4e9d7c7ccd7a5d7966aa448fb509b25da4b9aa51
kh-debug <kmhorton@uw.edu>
2021-02-08 20:19:45 -0800

    input username, database of other users

---
+++ src/NamePicker.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+function NamePicker(props){
+    const [showInput, setShowInput] = useState(false)
+    const [username, setUsername] = useState(
+        localStorage.getItem('username') || ''
+    )
+
+    function save(){
+        props.saveName(username)
+        setShowInput(false)
+        localStorage.setItem('username',username)
+    }
+    function keyPressed(e){
+        if(e.key==='Enter') {
+            save()
+        }
+    }
+
+    if (showInput) {
+        return <div className="name-picker">
+            <input value={username} className="input-user"
+                onChange={e=> setUsername(e.target.value)}
+                onKeyPress={keyPressed}
+            />
+            <button onClick={save} className="save-user">✓</button>
+        </div>
+    }
+
+    return <div className="name-picker">
+        <div>{username}</div>
+        <button onClick={()=> setShowInput(true)} className="edit-user">✎</button>
+    </div>
+}
+
+export default NamePicker
